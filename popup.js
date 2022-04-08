a6722b6a4ab17c19b55e39ecb7ad0af50d88e396
sakaki <sakajun02080@gmail.com>
2022-04-08 19:05:37 +0900

    update 0407

--- popup.js
+++ popup.js
@@ -2,73 +2,36 @@
 //変数宣言
 let checkValue = '1';
 const storage = chrome.storage.sync;
-document.addEventListener('change', function() {
 
-  // function key_storage() {
-  //   for (let i = 0; i < 6; i++) {
-  //     chrome.storage.sync.set({i : ''});
-  //     console.log("test");
-  //   }
-  // }
-  // key_storage()
+document.addEventListener('click', () => {
 
   const RadioButton = document.getElementById("RadioButton");
   const Radio = document.getElementsByName("radio1");
   let len = Radio.length;
 
-
-
   //テキスト取得
   let element = document.getElementById('text');
 
-
-
   console.log(element.value);
+  console.log(checkValue);
 
   //情報の登録
-  chrome.storage.local.set({checkValue: element});
-
-  //console.log(checkValue);
-
-  // storage.set({checkValue: element.value}, function() {
-  //   storage.get(function(e) {
-  //     console.log(e);
-  //   });
-  // });
-
-    //ラジオボタン番号取得
-    // for (let i = 1; i <= len; i++) {
-
-    //     checkValue = i;
-    //     chrome.storage.sync.set({checkValue: checkValue});
-    // };
-
-
-
-  //ラジオボタン番号取得
-  // for (let i = 0; i < len; i++) {
-  //   if(Radio.item(i).checked){
-  //     checkValue = Radio.item(i).value;
-  //     chrome.storage.sync.set({checkValue: checkValue})
-  //   }
-  // }
+  chrome.storage.sync.set({checkValue: element.value}, function() {
+    console.log(checkValue);
+    console.log('Value is set to '  + element.value + ':' + checkValue);
+  });
 
   for (let i = 0; i < len; i++) {
     if(Radio.item(i).checked){
       checkValue = Radio.item(i).value;
-      chrome.storage.local.get(checkValue, function(result){
+      chrome.storage.sync.get(checkValue, ({ result }) => {
         console.log('Value currently is ' + result);
       })
     }
   }
 
-  // chrome.storage.local.set({key: "aaa"}, function() {
-  //   console.log('Value is set to ' + value);
-  // });
 
-  // chrome.storage.local.get(['key'], function(result) {
-  //   console.log('Value currently is ' + result.key);
-  // });
+
 
   //console.log(checkValue);
 
