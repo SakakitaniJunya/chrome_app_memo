9aad2a105006cc8d2643c8f6e4722adf4e56349c
sakaki <sakajun02080@gmail.com>
2022-04-10 15:43:54 +0900

    update

--- popup.js
+++ popup.js
@@ -5,6 +5,9 @@ const storage = chrome.storage.sync;
 
 document.addEventListener('click', () => {
 
+// storage.syncの場合
+chrome.storage.sync.get(null, ((data) => {console.log(data)}));
+
   const RadioButton = document.getElementById("RadioButton");
   const Radio = document.getElementsByName("radio1");
   let len = Radio.length;
@@ -16,17 +19,20 @@ document.addEventListener('click', () => {
   console.log(checkValue);
 
   //情報の登録
-  chrome.storage.sync.set({checkValue: element.value}, function() {
-    console.log(checkValue);
-    console.log('Value is set to '  + element.value + ':' + checkValue);
+  chrome.storage.sync.set({[checkValue]: element.value}, function(result) {
+    console.log(result);
+    console.log('Value is set to ' + ':' + result);
   });
 
   for (let i = 0; i < len; i++) {
     if(Radio.item(i).checked){
       checkValue = Radio.item(i).value;
-      chrome.storage.sync.get(checkValue, ({ result }) => {
-        console.log('Value currently is ' + result);
-      })
+      // chrome.storage.sync.get(checkValue, ({ result }) => {
+      //   console.log('Value currently is ' + result);
+      // })
+      chrome.storage.sync.get({'value': element.value}, function() {
+        console.log('Settings saved');
+    });
     }
   }
 
